89219ea2ed01530cf3fb39f6bbb31b3119de8c93
Hilary Castelar <108029132+castelarxxx@users.noreply.github.com>
2023-08-07 21:41:57 -0300

    Add files via upload

--- matriz.py
+++ matriz.py
@@ -1,21 +1,101 @@
-matriz = [
-    [1, "a", 2],
-    ["b", 3, 4],
-    [6, 5, "c"]
-]
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
 
-print(matriz[0])  # [1, "a", 2]
+print(matriz[0])  # (1, "a", 2)
 print(matriz[0][0])  # 1
 print(matriz[0][-1])  # 2
 print(matriz[-1][-1])  # "c"
 
 
-#fatiamento
-lista = ["p", "y", "t", "h", "o", "n"]
+#FATIAMENTO
 
-print(lista[2:])  # ["t", "h", "o", "n"]
-print(lista[:2])  # ["p", "y"]
-print(lista[1:3])  # ["y", "t"]
-print(lista[0:3:2])  # ["p", "t"]
-print(lista[::])  # ["p", "y", "t", "h", "o", "n"]
-print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"]
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+print(tupla[2:])  # ("t", "h", "o", "n")
+print(tupla[:2])  # ("p", "y")
+print(tupla[1:3])  # ("y", "t")
+print(tupla[0:3:2])  # ("p", "t")
+print(tupla[::])  # ("p", "y", "t", "h", "o", "n")
+print(tupla[::-1])  # ("n", "o", "h", "t", "y", "p")
+
+#ITERAR
+carros = (
+    "gol",
+    "celta",
+    "palio",
+)
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+    
+    
+    
+#COMPREENSAO DE LISTAS
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+
+#APPEND
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+#CLEAR
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
+
+
+#COPY
+lista = [1, "Python", [40, 30, 20]]
+
+lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+
+#COUNT
+cores = ["vermelho", "azul", "verde", "azul"]
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
+
+#EXTEND
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
+
+
+#INDEX
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
+
---
+++ tuplas.py
@@ -0,0 +1,16 @@
+#acesso direto
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+#indices negativos
+frutas = (
+    "maçã",
+    "laranja",
+    "uva",
+    "pera",
+)
+
+print(frutas[-1])  # pera
+print(frutas[-3])  # laranja
